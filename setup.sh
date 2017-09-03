a288791599b87d1eb7443579260c193151cf342e
Rom Valme <rom.valme@gmail.com>
2017-09-02 23:16:54 -0400

    adding list of installed tools

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ cp .tmux.conf ~
 echo 'eval `dircolors ~/.dir_colors`' >> ~/.bashrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+#install ag, vim, tmux
